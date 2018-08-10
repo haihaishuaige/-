0d4cd2f02ae9d24f23e60fbf26b8acbd10d281ca
haihaishuaige <184805761@qq.com>
2018-08-10 19:09:38 +0800

    首页渲染成功，分类开始结构

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@ $(function () {
 
 
     function init() {
-        header_slide();
-
+        getSwiperData();
+        getCatitems();
+        getGoodslist();
     }
     // 发送ajax请求轮播图的数据   http://api.pyg.ak48.xyz/api/public/v1/home/swiperdata
-    function header_slide() {
-        $.get("http://api.pyg.ak48.xyz/api/public/v1/home/swiperdata", function (res) {
+    function getSwiperData() {
+        $.get("home/swiperdata", function (res) {
             if (res.meta.status == 200) {
                 var slides_html = template("header_slides", {
                     arr: res.data
@@ -19,6 +20,29 @@ $(function () {
                 gallery.slider({
                     interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
                 });
+            }
+        })
+    }
+    // 发送ajax请求四个分类的图标 http://api.pyg.ak48.xyz/api/public/v1/home/catitems
+    function getCatitems(){
+        $.get('home/catitems',function(res){
+            // console.log(res);
+            if(res.meta.status==200){
+                var html = template("four_icons",{arr:res.data});
+                $(".four_fenlei").html(html)
+            }   
+        })
+    }
+    //发送ajax请求主体商品内容 http://api.pyg.ak48.xyz/api/public/v1/home/goodslist
+    function getGoodslist(){
+        $.get("home/goodslist",function(res){ 
+            if(res.meta.status==200){
+                var html = template("view_banner",{arr:res.data})
+                // console.log(res);
+                
+                $(".view_shopping").html(html)
+                
+                
             }
         })
     }
