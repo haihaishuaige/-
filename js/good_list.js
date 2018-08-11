bd10682ab10305bc0baaccbf5fd797d31e979934
haihaishuaige <184805761@qq.com>
2018-08-11 21:51:09 +0800

    实现简单上拉下拉

--- js/good_list.js
+++ js/good_list.js
@@ -1,1 +1,55 @@
-  
+$(function(){
+    var GetSearchObj = {
+        query:'',
+        cid:getURL('cid'),
+        pagenum:1,
+        pagesize:10
+    }
+    var contents;
+    init();
+    mui.init({
+        pullRefresh: {
+          container: ".pyg_view",
+          down: {
+            auto: true,
+            //  触发下拉刷新时自动触发
+            callback: function () {
+                $(".view_banner ul").html('');
+                init(function(){
+                    // console.log(contents) 4;
+                    mui('.pyg_view').pullRefresh().endPulldownToRefresh();
+                });    
+            }
+          },
+          up:{
+            //  触发上拉刷新时自动触发
+            callback:function () {
+              init(function(){
+                mui('.pyg_view').pullRefresh().endPullupToRefresh();
+              });
+            }
+          }
+        }
+      });
+    function init(callback){
+       //主体内容渲染  goods/search
+       $.get("goods/search",GetSearchObj,function(res){
+          if(res.meta.status==200){
+            contents = Math.ceil(res.data.total / (GetSearchObj.pagesize));
+                var html = template("view_banner",{arr:res.data.goods})
+                 $(".view_banner ul").append(html);
+                 callback&&callback();
+          }       
+       })
+
+    }
+
+
+    //获取url地址参数
+     function getURL(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]);
+        return null;
+      }
+})  
