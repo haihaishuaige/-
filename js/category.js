aeed01a7b8912c439b9fc0ed945b06ce1c4cbfd8
haihaishuaige <184805761@qq.com>
2018-08-11 08:36:36 +0800

    分类右边动态渲染完成

--- js/category.js
+++ js/category.js
@@ -23,10 +23,12 @@ $(function(){
         leftScroll = new IScroll(".pyg_view .left");
         
     }
+    var dataId = dataId || 0;
     //获得商品信息   goods/qsearch
     function getCategories(){
         $.get("categories",function(res){
             //左边导航栏
+            res = res;
             var htmlLeft = template("left_banner",{arr:res.data});
             $(".left ul").html(htmlLeft);
             Scroll();
@@ -34,26 +36,37 @@ $(function(){
                 background:'#fff',
                 borderLeft:'5px solid #0094ff'
             })
-            var htmlRight = template("right_banner",{data:res.data[0]});
-            $(".right ul").html(htmlRight);
-        //   console.log(res.data);
-          
-            
-            
+            //渲染右边数据
+            var htmlRight = template("right_banner",{data:res.data[dataId]});
+            $(".right ul").html(htmlRight);   
+          //获取当前点击的事件 
+          var page =  $('.left ul li').eq(dataId)[0];
+          $('.left ul li').eq(dataId).css({
+            background:'#fff',
+            borderLeft:'5px solid #0094ff'
+        }).siblings().css({
+            background:'',
+            borderLeft:'none'
+        });
+        haha(page);
         })
     }
+    //左边点击事件
     $(".left ul").on('tap','li',function(e){
-        // console.log(e.target);
-        leftScroll.scrollToElement(e.target,1500);
-       
+        var page = e.target;
+        var value =$(this).children('input').val();
+        value = value -1 + 1;
+        dataId = value;
+        getCategories();  
       $(this).css({
           background:'#fff',
           borderLeft:'5px solid #0094ff'
       }).siblings().css({
           background:'',
           borderLeft:'none'
-      });
-       
-        
+      });      
     })
+    function haha(page){
+        leftScroll.scrollToElement(page,500);
+    }
 })
