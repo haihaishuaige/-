99eb0c088bb9fb097d54aa9b976629a79d4cdda6
haihaishuaige <184805761@qq.com>
2018-08-15 11:54:49 +0800

    动态获取价钱完成，准备开始删除按钮

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
  $(function () {
      init();
-   
+
      function init() {
          //先判断用户是否登陆 
          if (!$.checktoken()) {
@@ -12,34 +12,86 @@
          $("body").fadeIn(200);
          getAll();
          editBtn();
+
      }
      //发送ajax my/cart/all
-     function getAll(){
+     function getAll() {
          var token = $.token();
-        $.ajax({
-            url:"my/cart/all",
-            headers:{Authorization:token},
-            type:'get',
-            success:function(res){
-               if(res.meta.status==200){
-                var data =JSON.parse(res.data.cart_info);
-                var html = template("pyg_main",{data:data});
-                $(".pyg_shopping ul").html(html);
-                //初始化数字框
-                mui('.mui-numbox').numbox()
-               }    
-            }
-        })
-     } 
-     function editBtn(){
-         $(".edit_btn").on('tap',function(){
-             $('body').toggleClass("big_box");
-             if($('body').hasClass('big_box')){
-                 $(this).text('完成')
-                  $(".pyg_shopping_img").css('padding','0')  
-             }else{
-                $(this).text('编辑');
+         $.ajax({
+             url: "my/cart/all",
+             headers: {
+                 Authorization: token
+             },
+             type: 'get',
+             success: function (res) {
+                 // console.log(JSON.parse(res.data.cart_info));   
+                 if (res.meta.status == 200) {
+                     var data = JSON.parse(res.data.cart_info);
+                     var html = template("pyg_main", {
+                         data: data
+                     });
+                     $(".pyg_shopping ul").html(html);
+                     //初始化数字框
+                     mui('.mui-numbox').numbox()
+                     prices();
+                 } else {
+                     localStorage.setItem("location_url", location.href);
+                     location.href = "/pages/login.html";
+                     return;
+                 }
              }
          })
      }
+     //切换按钮
+     function editBtn() {
+         $(".edit_btn").on('tap', function () {
+             $('body').toggleClass("big_box");
+             if ($('body').hasClass('big_box')) {
+                 $(this).text('完成')
+                 $(".pyg_shopping_img").css('padding', '0')
+             } else {
+                 $(this).text('编辑');
+             }
+         })
+     }
+     //点击动态计算价格
+     $('.pyg_shopping').on('tap', '.mui-btn', function () {
+         prices();
+     })
+
+     function prices() {
+         //获取所有li的data数据  循环遍历
+         var inps = $('.mui-numbox-input');
+         var lis = document.querySelectorAll('.pyg_shopping li');
+         var prices_nums = 0;
+         var li = [];
+         var vals = [];
+         for (var i = 0; i < inps.length; i++) {
+             vals.push($(inps[i]).val());
+              li.push($(lis[i]).data('price'));
+         } 
+         var arr = [];
+         $.each(li, function (k, v) {
+             arr.push(v.goods_price)
+         })
+         for (var i = 0; i < arr.length; i++) {
+             prices_nums += vals[i] * arr[i];
+         }
+         $('.pyg_dingdan_price_left span').text(prices_nums)
+     }
+     //点击删除按钮
+     $('.delete_btn').on('tap',function(){
+         var arr2 = [];
+         //找到勾选上的li
+         var del_check = $('.pyg_shopping [name="check_value"]:checked');
+         //.data('price')  .getAttribute('data-price')
+        var check_li = del_check.parents('li');
+        for(var i = 0;i<$(check_li).length;i++){
+            arr2.push($(check_li)[i].getAttribute('data-price'));  
+        }  
+        //发送ajax请求 Authorization
+        var token = $.token();      
+        
+            
+     })
  })
